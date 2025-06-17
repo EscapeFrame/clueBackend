783aeb8c0e49d143f202c3e56be6855ef385a563
Hgyeol <neunimchi626@gmail.com>
2025-06-17 23:33:06 +0900

    feat(#2): /my 수정

--- src/main/java/hello/cluebackend/domain/user/presentation/MyController.java
+++ src/main/java/hello/cluebackend/domain/user/presentation/MyController.java
@@ -1,13 +1,28 @@
 package hello.cluebackend.domain.user.presentation;
 
+import hello.cluebackend.global.config.JWTUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
+    private final JWTUtil jwtUtil;
+
+    public MyController(JWTUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
     @GetMapping("/my")
-    public String myAPI() {
-        return "my route";
+    public String myAPI(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+
+            String username = jwtUtil.getUsername(token);
+            return username;
+        }
+        return "No JWT token found";
     }
 }
