d5b3d16387ed32b4ee81ce347e494c629d58ab2a
fixgramwork <kimminje661@outlook.kr>
2025-07-09 09:03:50 +0900

    feat(#14): 값 검증 추가

--- src/main/java/hello/cluebackend/domain/timetable/presentation/TimetableController.java
+++ src/main/java/hello/cluebackend/domain/timetable/presentation/TimetableController.java
@@ -4,6 +4,8 @@ import hello.cluebackend.domain.timetable.presentation.dto.request.TimetableRequ
 import hello.cluebackend.domain.timetable.presentation.dto.response.TimetableResponseDto;
 import hello.cluebackend.domain.timetable.service.TimetableService;
 import lombok.RequiredArgsConstructor;
+
+import jakarta.validation.constraints.Pattern;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -18,8 +20,8 @@ public class TimetableController {
 
   @GetMapping("/today")
   public Mono<ResponseEntity<List<TimetableResponseDto>>> getTodayTimetable(
-          @RequestParam(required = true) String grade,
-          @RequestParam(required = true) String classNumber
+          @RequestParam(required = true) @Pattern(regexp = "^[1-3]$", message="학년은 1~3 사이여야 합니다.") String grade,
+          @RequestParam(required = true) @Pattern(regexp = "^[1-4]$", message="반은 1~4 사이여야 합니다.") String classNumber
 ) {
     TimetableRequestDto request = new TimetableRequestDto(grade, classNumber);
     return timetableService.getTodayTimetable(request)
