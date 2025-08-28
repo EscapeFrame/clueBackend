9fb9e9c60528ae881e4cb7381c9614a4b876fafb
Haeyul <kimminje661@outlook.kr>
2025-08-28 17:05:30 +0900

    feat(#5): aws region 수정

--- src/main/java/hello/cluebackend/global/config/S3Config.java
+++ src/main/java/hello/cluebackend/global/config/S3Config.java
@@ -16,7 +16,7 @@ public class S3Config {
   @Value("${cloud.aws.credentials.secret-key}")
   private String secretKey;
 
-  @Value("{cloud.aws.credentials.static}")
+  @Value("${cloud.aws.region.static}")
   private String region;
 
   @Bean
