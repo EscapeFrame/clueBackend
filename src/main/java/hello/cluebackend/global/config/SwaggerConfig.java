a9e49e4db1ef7491e854567d510c3b3f9051a4c3
Hgyeol <neunimchi626@gmail.com>
2025-08-31 23:35:53 +0900

    feat(#8) : swagger title 변경

--- src/main/java/hello/cluebackend/global/config/SwaggerConfig.java
+++ src/main/java/hello/cluebackend/global/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
 
     private Info apiInfo() {
         return new Info()
-                .title("CodeArena Swagger")
+                .title("Clue Swagger")
                 .description("CLUE REST API")
                 .version("1.0.0");
     }
