265b0aed56d4d750c1aab37e07aea1839d8d1724
Hgyeol <neunimchi626@gmail.com>
2025-09-18 20:34:56 +0900

    bug(#84): 임시 /me 추가

--- src/main/java/hello/cluebackend/domain/user/presentation/RegisterController.java
+++ src/main/java/hello/cluebackend/domain/user/presentation/RegisterController.java
@@ -1,25 +1,29 @@
 package hello.cluebackend.domain.user.presentation;
 
+import hello.cluebackend.domain.user.domain.Role;
 import hello.cluebackend.domain.user.presentation.dto.DefaultRegisterUserDto;
 import hello.cluebackend.domain.user.presentation.dto.RegisterUserDto;
 import hello.cluebackend.domain.user.presentation.dto.UserDto;
 import hello.cluebackend.domain.user.service.UserService;
+import hello.cluebackend.global.config.JWTUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class RegisterController {
-    private final UserService userService;
 
-    public RegisterController(UserService userService) {
-        this.userService = userService;
-    }
+    private final JWTUtil jwtUtil;
+    private final UserService userService;
 
     @GetMapping("/first-register")
     public DefaultRegisterUserDto showRegistrationForm(HttpServletRequest request) {
@@ -41,5 +45,25 @@ public class RegisterController {
         log.info("ClassCode 1 : " + registerUserDto.getClassCode());
         userService.registerUser(userDto, registerUserDto.getClassCode());
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<userData> getCurrentUser(HttpServletRequest request) {
+        String token = jwtUtil.getToken(request);
+        UUID userId = jwtUtil.getUserId(token);
+        Role role = jwtUtil.getRole(token);
+        String username = jwtUtil.getUsername(token);
+
+        return ResponseEntity.status(HttpStatus.OK).body(new userData(userId, username, role));
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    static class userData {
+        private UUID userId;
+        private String username;
+        private Role role;
     }
 }
