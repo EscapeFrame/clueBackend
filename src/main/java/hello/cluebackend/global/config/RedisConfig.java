334966ab6c7aebe9bc13b25e6ea9d7ddd924faa8
Haeyul <kimminje661@outlook.kr>
2025-09-24 17:42:52 +0900

    bug(#86): Redis Json 직렬화 역직렬화 Config 추가

--- src/main/java/hello/cluebackend/global/config/RedisConfig.java
+++ src/main/java/hello/cluebackend/global/config/RedisConfig.java
@@ -9,6 +9,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -30,17 +31,15 @@ public class RedisConfig {
     RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
     redisTemplate.setConnectionFactory(redisConnectionFactory());
 
-    // 일반적인 key:value의 경우 시리얼라이저
     redisTemplate.setKeySerializer(new StringRedisSerializer());
     redisTemplate.setValueSerializer(new StringRedisSerializer());
 
-    // Hash를 사용할 경우 시리얼라이저
     redisTemplate.setHashKeySerializer(new StringRedisSerializer());
     redisTemplate.setHashValueSerializer(new StringRedisSerializer());
 
-    // 모든 경우
-    redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
+    redisTemplate.setDefaultSerializer(new StringRedisSerializer());
     return redisTemplate;
   }
 }
