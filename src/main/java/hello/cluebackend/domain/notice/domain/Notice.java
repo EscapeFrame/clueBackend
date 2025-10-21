e25aa2b2d0fb134ce050cc7c8ca6dc80ea462cfc
Hgyeol <neunimchi626@gmail.com>
2025-10-22 01:32:28 +0900

    공지사항 엔티티 추가

---
+++ src/main/java/hello/cluebackend/domain/notice/domain/Notice.java
@@ -0,0 +1,44 @@
+package hello.cluebackend.domain.notice.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import hello.cluebackend.domain.classroom.domain.ClassRoom;
+import hello.cluebackend.domain.user.domain.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notice")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Notice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "class_room_id")
+    private ClassRoom classRoom;
+}
