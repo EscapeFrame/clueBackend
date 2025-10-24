ebce286f8b80adaab1229806e6f3b62426b99752
Haeyul <kimminje661@outlook.kr>
2025-10-24 10:28:56 +0900

    fix(#103): service entitiy 반환 controller dto 반환

--- src/main/java/hello/cluebackend/domain/submission/service/SubmissionCommandService.java
+++ src/main/java/hello/cluebackend/domain/submission/service/SubmissionCommandService.java
@@ -38,7 +38,7 @@ public class SubmissionCommandService {
   // 과제 전체 조회 및 과제 첨부 파일 조회
   public List<SubmissionResponse> findAllByAssignmentId(UUID userId, UUID classId) {
     UserEntity user = userService.findById(userId).toEntity();
-    ClassRoom classRoom = classRoomService.findById(userId, classId).toEntity();
+    ClassRoom classRoom = classRoomService.findById(userId, classId);
 
     List<Submission> submissions = submissionRepository.findAllByClassRoomAndUser(classRoom, user);
 
