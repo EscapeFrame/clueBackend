55d2815549dbceda561fd1a1384c44f790d76a52
Hgyeol <neunimchi626@gmail.com>
2025-11-18 10:34:43 +0900

    assignment updateDetails에서 null 비교하는 로직 삭제

--- src/main/java/hello/cluebackend/application/assignment/dto/request/ModifyAssignmentDto.java
+++ src/main/java/hello/cluebackend/application/assignment/dto/request/ModifyAssignmentDto.java
@@ -2,6 +2,7 @@ package hello.cluebackend.application.assignment.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -11,8 +12,12 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class ModifyAssignmentDto {
+  @NotNull
   private String title;
+  @NotNull
   private String content;
+  @NotNull
   private @JsonProperty("start_date") @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul") LocalDateTime startDate;
+  @NotNull
   private @JsonProperty("end_date") @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul") LocalDateTime endDate;
 }
