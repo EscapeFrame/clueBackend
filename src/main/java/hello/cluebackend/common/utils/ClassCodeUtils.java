f03fc0ca4f1abc89168b4faa676c91de8c8560fe
Hgyeol <neunimchi626@gmail.com>
2025-11-19 13:55:08 +0900

    유저 학반 구하는 로직에서 UserEntity.getGrade, UserEntity.getClassNo로 가져오는 걸로 변경

--- src/main/java/hello/cluebackend/common/utils/ClassCodeUtils.java
+++ src/main/java/hello/cluebackend/common/utils/ClassCodeUtils.java
@@ -7,9 +7,8 @@ public class ClassCodeUtils {
   private ClassCodeUtils() {}
 
   public static String[] classCodeConverter(UserEntity user) {
-    int code = user.getClassCode();
-    String grade = String.valueOf(code / 1000);
-    String classNumber = String.valueOf((code % 1000) / 100);
+    String grade = String.valueOf(user.getGrade());
+    String classNumber = String.valueOf(user.getClassNo());
     return new String[]{grade, classNumber};
   }
 }
