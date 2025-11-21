2013f3b5a91127d298f9c673453b081bc0e87993
Hgyeol <neunimchi626@gmail.com>
2025-11-21 14:31:29 +0900

    submission 링크 추가할때 리스트로 받는 것으로 변경

--- src/main/java/hello/cluebackend/presentation/api/submission/SubmissionQueryController.java
+++ src/main/java/hello/cluebackend/presentation/api/submission/SubmissionQueryController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -70,7 +71,7 @@ public class SubmissionQueryController {
   public ResponseEntity<?> linkUpload(
           @CurrentUser UUID userId,
           @PathVariable UUID submissionId,
-          @RequestBody SubmissionAttachmentUrlDto dto
+          @RequestBody List<SubmissionAttachmentUrlDto> dto
   ) {
     submissionQueryService.linkUpload(userId, submissionId, dto);
     return ResponseEntity.ok("첨부 파일 삭제");
