3c8ff8444c8e8766925dbf3a989a5194bd3fb376
Haeyul <kimminje661@outlook.kr>
2025-11-23 15:11:38 +0900

    ref(#167): Agent Dto 값 수정

--- src/main/java/hello/cluebackend/application/agent/dto/response/AgentAllDataResponse.java
+++ src/main/java/hello/cluebackend/application/agent/dto/response/AgentAllDataResponse.java
@@ -1,21 +1,15 @@
 package hello.cluebackend.application.agent.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.UUID;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AgentAllDataResponse {
-  @JsonProperty("agent_id")
-  private UUID agentId;
-  private String status;
-  private Planning planning;
-  private Flow flow;
-  private Doc doc;
-  private AgentGraphResponse graph;
+  private Boolean success;
+  private String message;
+  private AllAgentData data;
+  private String error;
 }
