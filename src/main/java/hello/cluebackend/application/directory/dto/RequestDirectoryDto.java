a16898b0294f2327e3bf783d6bb48e91def4d96a
Haeyul <kimminje661@outlook.kr>
2025-12-01 16:19:58 +0900

    RQB-8 DirectoryDto에서 디렉토리 데이터 사용시 규칙 설정

--- src/main/java/hello/cluebackend/application/directory/dto/RequestDirectoryDto.java
+++ src/main/java/hello/cluebackend/application/directory/dto/RequestDirectoryDto.java
@@ -1,12 +1,20 @@
 package hello.cluebackend.application.directory.dto;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 import java.util.UUID;
 
 @Getter
 public class RequestDirectoryDto {
+    @Nullable
     private UUID directoryId;
+
+    @NotNull(message="클래스룸 ID는 필수입니다.")
     private UUID classRoomId;
+
+    @NotBlank(message = "디렉토리 이름은 필수입니다.")
     private String name;
 }
